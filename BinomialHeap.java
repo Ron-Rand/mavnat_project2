a4390df18d9521fc38a327fb9dbd65d87f1a8401
Ron-Rand <97893474+Ron-Rand@users.noreply.github.com>
2024-07-17 23:38:13 +0300

    Update BinomialHeap.java meld&decreasekey&connect

--- BinomialHeap.java
+++ BinomialHeap.java
@@ -59,6 +59,17 @@ public class BinomialHeap
 	 */
 	public void decreaseKey(HeapItem item, int diff)
 	{
+		item.key -= diff;
+		while (item.node.parent != null && item.key < item.node.parent.item.key){
+			int temp = item.key;
+			item.key = item.node.parent.item.key;
+			item.node.parent.item.key = temp;
+			item = item.node.parent.item;
+		}
+		if (item.key < this.min.item.key){
+			this.min = item.node;
+		}
+
 		return; // should be replaced by student code
 	}
 
@@ -80,6 +91,57 @@ public class BinomialHeap
 	 */
 	public void meld(BinomialHeap heap2)
 	{
+		if (heap2.size == 0){
+			return;
+		}
+		this.size += heap2.size;
+		if (this.min.item.key > heap2.min.item.key){
+			this.min = heap2.min;
+		}
+
+		HeapNode heap1prev = this.last;
+		HeapNode heap1Curr = this.last.next;
+		HeapNode heap2Curr = heap2.last.next;
+
+		boolean endFlag = false;
+
+		for (int i = 0; i<heap2.numTrees() || !endFlag ;i++){
+			while (heap1Curr.rank>= heap2Curr.rank || heap1Curr.next.rank < heap1prev.next.rank ){
+				heap1prev = heap1prev.next;
+				heap1Curr = heap1Curr.next;
+			}
+			if (heap1Curr.rank < heap2Curr.rank){
+				heap1Curr.next = heap2Curr;
+				heap2Curr.next = this.last.next;
+				endFlag = true;
+			}
+
+			else if (heap1Curr.rank>heap2Curr.rank){
+				heap1prev.next = heap2Curr;
+				heap2Curr.next = heap2Curr;
+
+				heap2Curr = heap2Curr.next;
+			}
+			else if (heap1Curr.rank == heap2Curr.rank){
+				if (heap1Curr.item.key <= heap2Curr.item.key){
+					heap1Curr.connect(heap2Curr);
+					heap2Curr = heap2Curr.parent.next;
+				}
+				else{
+					HeapNode temp = heap2Curr.next;
+
+					heap1prev.next = heap2Curr;
+					heap2Curr.next = heap1Curr.next;
+					heap2Curr.connect(heap1Curr);
+
+					heap2Curr = temp;
+				}
+			}
+		}
+
+
+
+
 		return; // should be replaced by student code
 	}
 
@@ -132,6 +194,17 @@ public class BinomialHeap
 		public HeapNode next;
 		public HeapNode parent;
 		public int rank;
+
+		private void connect(HeapNode node2){
+			HeapNode temp1 = this.child;
+			node2.parent = this;
+			this.child = node2;
+			HeapNode temp2 = temp1.next;
+			temp1.next = node2;
+			node2.next = temp2;
+			this.rank++;
+			return;
+		}
 	}
 
 	/**
